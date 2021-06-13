057455933cc86ebf901d432dd014e1d573993ae1
Noelia <noinsanc@espol.edu.ec>
2021-06-13 13:38:38 -0500

    Update calculadora.asm

--- calculadora.asm
+++ calculadora.asm
@@ -1,5 +1,14 @@
 .data
-
-
+sueldo: .asciiz "Ingrese su sueldo actual: $"
+hijos: .asciiz "Ingrese la cantidad de hijos: "
+newLine: .asciiz "\n"
 
 .text
+.globl main
+
+main:
+
+
+fin: 
+	li $v0, 10
+	syscall
